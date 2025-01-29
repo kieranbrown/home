1965549e32d852b759a50efe01ab64d514de10c6
Kieran Brown <kieran.brown@chatloop.com>
2025-01-29 21:07:35 +0000

    ci: added digger and imported resources into tf (#1)

--- terraform/cloudflare-access-settings/outputs.tf
+++ terraform/cloudflare-access-settings/outputs.tf
@@ -6,6 +6,10 @@ output "app_policy_bypass_id" {
   value = cloudflare_zero_trust_access_policy.bypass.id
 }
 
+output "app_policy_public_id" {
+  value = cloudflare_zero_trust_access_policy.public.id
+}
+
 output "idp_google_id" {
   value = data.cloudflare_zero_trust_access_identity_provider.google.id
 }
